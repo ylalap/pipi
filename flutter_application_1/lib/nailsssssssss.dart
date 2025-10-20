d76c6c288ccac1a4c55c40e645e727060257553e
ylalap <laylakv1@icloud.com>
2025-10-20 14:35:39 +0000

    a

--- flutter_application_1/lib/nailsssssssss.dart
+++ flutter_application_1/lib/nailsssssssss.dart
@@ -18,97 +18,233 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // Заголовок
-          Container(
-            width: double.infinity,
-            height: 100,
-            color: Colors.pink[100],
-            child: Center(
-              child: Text(
-                'Салон красоты "Nailsss"',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.purple,
+      body: SingleChildScrollView(
+        // direction: Axis.vertical, // По умолчанию вертикальная прокрутка
+        child: Column(
+          children: [
+            // Заголовок
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.pink[100],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  'Салон красоты "Nailsss"',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
                 ),
               ),
             ),
-          ),
-          
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // Информация о мастере
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundImage: NetworkImage(
-                        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fjpg%2F&psig=AOvVaw2qEkWS1ipuYKW_n4ZoqFUP&ust=1760712225419000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIjsmqn6qJADFQAAAAAdAAAAABAE', 
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'Анна Петрова',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
+            
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // Информация о мастере
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 1,
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
                         ),
-                        Text('Мастер маникюра'),
-                        Text('Опыт: 5 лет'),
                       ],
                     ),
-                  ],
-                ),
-                
-                // Услуги
-                Container(
-                  width: 300,
-                  height: 150,
-                  color: Colors.purple[50],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        'Наши услуги:',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                            'https://avatars.mds.yandex.net/get-yapic/27232/2QFrz3lyfCtG4z1lnfoDbaI-1/orig', 
+                          ),
                         ),
-                      ),
-                      Text('• Классический маникюр'),
-                      Text('• Аппаратный маникюр'),
-                      Text('• Покрытие гель-лаком'),
-                      Text('• Дизайн ногтей'),
-                    ],
+                        Column(
+                          children: [
+                            Text(
+                              'Анна Петрова',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.purple[800],
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              'Мастер маникюра',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              'Опыт: 5 лет',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                
-                // Контактная информация
-                Container(
-                  width: double.infinity,
-                  height: 80,
-                  color: Colors.pink[50],
-                  child: Center(
-                    child: Text(
-                      'Запись по телефону: +7 (999) 123-45-67',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                  
+                  // Услуги
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.purple[50],
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                        color: Colors.purple[100]!,
+                        width: 1,
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          'Наши услуги:',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple[800],
+                          ),
+                        ),
+                        SizedBox(height: 12),
+                        Text(
+                          '• Классический маникюр',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        Text(
+                          '• Аппаратный маникюр',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        Text(
+                          '• Покрытие гель-лаком',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        Text(
+                          '• Дизайн ногтей',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                  SizedBox(height: 20),
+                  
+                  // Контактная информация
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.pink[50],
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.pink[50]!,
+                          Colors.purple[50]!,
+                        ],
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Запись по телефону: +7 (999) 123-45-67',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.purple[800],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+
+                  // Дополнительный контент для демонстрации прокрутки
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 1,
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Дополнительная информация',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple[800],
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Мы предлагаем качественные услуги маникюра с использованием современных материалов и технологий. Наши мастера регулярно проходят обучение и следят за последними тенденциями в индустрии красоты.',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[700],
+                            height: 1.4,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       
       floatingActionButton: FloatingActionButton(
